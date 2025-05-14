cc8a4c4257fc2a546a56515d45b4dff717b5c584
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-05-14 13:57:22 -0300

    Inicio

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Inicio');
+
+    const wrappersForm = document.querySelector('.wrappers__form');
+    const wrappersNames = document.querySelector('.wrappers__names');
+    const wrappersMail = document.querySelector('.wrappers__mail');
+    const querysTypes = document.querySelector('.querys__types');
+    const wrappersMessage = document.querySelector('.wrappers__message');
+    const wrappersConsent = document.querySelector('.wrappers__consent');
+
+
+
+
+
+});
